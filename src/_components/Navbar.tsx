8c12fa8b78da17e694b1fa30a9b4b5f10263fc0d
Sagartripathi <tripathisagar1997@gmail.com>
2025-06-22 20:56:00 +0545

    chore: add shadcn

--- src/_components/Navbar.tsx
+++ src/_components/Navbar.tsx
@@ -4,10 +4,6 @@ const Navbar = () => {
   return (
     <div className="flex items-center justify-between bg-black p-4 text-white">
       <div className="text-2xl font-bold">Airbook</div>
-      <div className="flex items-center gap-4">
-        <button>Home</button>
-        <button>About</button>
-      </div>
     </div>
   );
 };
