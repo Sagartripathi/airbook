4124d09461665a99259264027eda4bd03ae498d9
Sagartripathi <tripathisagar1997@gmail.com>
2025-06-25 13:31:30 +0545

    update: searchbar add

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import DisplayResults from "./_components/DisplayResults";
+import FindFlight from "./_components/FindFlight";
 import Navbar from "./_components/Navbar";
 import SearchBar from "./_components/SearchBar";
 
@@ -6,6 +8,7 @@ function App() {
     <>
       <Navbar />
       <SearchBar />
+      <DisplayResults />
     </>
   );
 }
